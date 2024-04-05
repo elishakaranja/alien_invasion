1ea766904b31d8155de065272fb4c6862b5ac890
elishakaranja <145563815+elishakaranja@users.noreply.github.com>
2024-04-05 13:54:15 +0300

    "Added Ship class and clock to AlienInvasion class; updated AlienInvasion's **init** method and game loop to include new ship"
    The changes include:
    
    * Importing the `Ship` class from the `ship` module.
    * Adding a `Ship` instance variable `self.ship` to the `AlienInvasion` class.
    * Creating a `pygame.time.Clock` instance variable `self.clock` in the `AlienInvasion` class.
    * Updating the `__init__` method of the `AlienInvasion` class to initialize the `self.ship` variable and the `self.clock` variable.
    * Calling the `blitme` method of the `self.ship` instance in the game loop.
    * Limiting the game loop to run at a maximum of 60 frames per second using the `tick` method of the `self.clock` instance.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,18 +1,23 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior. """
-    
+        
     def __init__(self):
         """Initialize the game and create game resources"""
         pygame.init()   
         self.settings = Settings()
+        
+        self.clock = pygame.time.Clock()
         # Set up the display
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)
         
         # set background color
         self.bg_color = self.settings.bg_color
@@ -27,9 +32,12 @@ class AlienInvasion:
                     
             # Redraw the screen during each pass through loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()     
                     
             # Make the mostrecently drawn screen visible 
             pygame.display.flip()
+            # Limit to 60
+            self.clock.tick(60)
             
 if __name__ =='__main__':
     # Make a game instance, and run the game.
